fc7b355993fbf5680d515d50900d5f7b5c66ad8c
Aleksandra Szlacheta <aleksandra.szlacheta@gmail.com>
2021-05-31 02:33:39 +0200

    Add ant design library (components library) and enable less loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,60 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new Dotenv({
-            path: path.resolve(__dirname, '.env'),
-            safe: true,
-        })
-    ]
+  mode: 'production',
+  entry: './src/index.jsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, '.env'),
+      safe: true,
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules|themes/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.less$/,
+        include: /node_modules|themes/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            },
+          }],
+      },
+    ],
+  },
 };
